2f50f761ef4ba09689b7be51040ae5f8b5361bd4
Ritik-the-dev <joshiritik865@gmail.com>
2025-01-29 09:13:52 +0530

    fixing cookie issue

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    domain: 'url-shoretner.vercel.app'
   })
 );
 
